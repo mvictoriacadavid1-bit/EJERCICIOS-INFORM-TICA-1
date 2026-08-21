befa3f71e75a84b0034cbb8fecd416926d3e6cdc
mvictoriacadavid1-bit <mvictoria.cadavid1@udea.edu.co>
2026-08-20 20:16:55 -0500

    Add files via upload

---
+++ ejemplo1.py
@@ -0,0 +1,7 @@
+número = float(input("Ingrese un número: "))
+
+cuadrado = número**2
+cubo = número**3
+
+print(f"El cuadrado de {número} es {cuadrado}")
+print(f"El cubo de {número} es {cubo}")
---
+++ ejemplo2.py
@@ -0,0 +1,12 @@
+sueldo1 = float(input("Ingrese el salaria del empleado1: "))
+sueldo2 = float(input("Ingrese el salario del empleado2: "))
+sueldo3 = float(input("Ingrese el salario del empleado3: "))
+
+nuevo_sueldo1 = sueldo1*1.10
+nuevo_sueldo2 = sueldo2*1.12
+nuevo_sueldo3 = sueldo3*1.15
+
+print(f"El nuevo salario del empleado 1 es $: {nuevo_sueldo1}")
+print(f"El nuevo salario del empleado 2 es $: {nuevo_sueldo2}")
+print(f"El nuevo salario del empleado 3 es $: {nuevo_sueldo3}")
+
